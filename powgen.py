774fcee3d7e967640b783b396cb550960e5a2e6a
Pål Grønås Drange <pgdr@equinor.com>
2020-02-21 14:17:25 +0100

    Add tick

--- powgen.py
+++ powgen.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 from collections import namedtuple
 import yaml
 
-Schedule = namedtuple("Schedule", "unit start stop")
+Schedule = namedtuple("Schedule", "name unit start end")
 
 
 @dataclass
@@ -31,6 +31,9 @@ class Device:
     activation: Activation
     profile: Profile
 
+    def compute(self, on_time):
+        return self.power
+
 
 class HeatCable:
     def __init__(self, power, temperature):
@@ -39,9 +42,24 @@ class HeatCable:
         )
         self.temperature = temperature
         self.heat = self.temperature
+        self.on = False
+        self.on_time = 0
 
     def tick(self):
-        pass
+        if self.on:
+            if self.heat > self.temperature * 1.05:
+                self.on = False
+                self.on_time = 0
+                return 0
+            else:
+                self.on_time += 1
+                return self.dev.compute(time=self.on_time)
+        else:
+            if self.heat < self.temperature * 0.95:
+                self.on = True
+                return self.dev.compute(time=self.on_time)
+            else:
+                return 0
 
 
 class Stove:
@@ -49,9 +67,16 @@ class Stove:
         self.dev = Device(
             power=power, activation=Activation.linear, profile=Profile.PWM
         )
+        self.on = False
+        self.on_time = 0
 
     def tick(self):
-        pass
+        if self.on:
+            self.on_time += 1
+            return self.dev.compute(time=self.on_time)
+        else:
+            self.on_time = 0
+            return 0
 
 
 class CoffeePot:
@@ -59,9 +84,16 @@ class CoffeePot:
         self.dev = Device(
             power=power, activation=Activation.boolean, profile=Profile.on
         )
+        self.on = False
+        self.on_time = 0
 
     def tick(self):
-        pass
+        if self.on:
+            self.on_time += 1
+            return self.dev.compute(time=self.on_time)
+        else:
+            self.on_time = 0
+            return 0
 
 
 class Charger:
@@ -69,9 +101,16 @@ class Charger:
         self.dev = Device(
             power=power, activation=Activation.sigmoid, profile=Profile.on
         )
+        self.on = False
+        self.on_time = 0
 
     def tick(self):
-        pass
+        if self.on:
+            self.on_time += 1
+            return self.dev.compute(time=self.on_time)
+        else:
+            self.on_time = 0
+            return 0
 
 
 def _parse_time(config):
@@ -96,7 +135,31 @@ def _parse_entry(name, config):
 
 def parse_schedule(sch):
     for name, cfg in sch.items():
-        yield {name: (_parse_entry(name, cfg), _parse_time(cfg))}
+        yield Schedule(name, _parse_entry(name, cfg), *_parse_time(cfg))
+
+
+def _initialize(schedule):
+    ns = []
+    for elt in schedule:
+        ns.append(elt)
+    return ns
+
+
+def _update(schedule, day, hour, minute):
+    total = 0
+    for elt in schedule:
+        total += elt.unit.tick()
+    return total
+
+
+def game(schedule, days=5):
+    schedule = _initialize(schedule)
+    for day in range(days):
+        # if day % 7 in (5, 6):   # weekend
+        for hour in range(24):
+            for minute in range(60):
+                power = _update(schedule, day, hour, minute)
+                print(power)
 
 
 def main():
@@ -107,7 +170,7 @@ def main():
 
     with open(argv[1]) as sch_file:
         schedule = list(parse_schedule(yaml.safe_load(sch_file)))
-    print(schedule)
+    print(game(schedule))
 
 
 if __name__ == "__main__":
